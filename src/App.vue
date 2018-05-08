4a7a3e12604afb0b6e0cf6ce8d7de4f0d8b5aa13
jsek <im68889094144@outlook.com>
2018-05-09 00:05:19 +0200

    [3] Result

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="app" v-cloak>
+        <environment-label></environment-label>
+        <h1>{{ title }}</h1>
+    </div>
+</template>
+
+<script>
+import EnvironmentLabel from './components/environment-label'
+export default {
+    name: 'app',
+    data() {
+        return {
+            title: 'Hello Vue.js + Webpack'
+        }
+    },
+    components: {
+        EnvironmentLabel
+    }
+}
+</script>
+
+<style>
+    [v-cloak] { display: none }
+</style>
---
+++ src/components/environment-label.vue
@@ -0,0 +1,33 @@
+<template>
+    <p v-if='isLocal' class="env-label dev">Localhost</p>
+    <p v-else class="env-label prod">Hostname: {{ hostname }}</p>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            isLocal: false,
+            hostname: '',
+        }
+    },
+    mounted() {
+        this.isLocal = /(127\.0\.0\.1)|(localhost)/.test(location.host);
+        this.hostname = location.hostname;
+    },
+}
+</script>
+
+<style scoped>
+.env-label {
+    color: #fff;
+    font-size: .9em;
+    padding: .4rem .8rem;
+    border-radius: 15px;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    text-align: center;
+}
+.dev  { background: #00bfa5; }
+.prod { background: #8c30a8; }
+</style>
