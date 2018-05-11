908a4dfb6cd8105531da82cb60dc9090efa07988
jsek <im68889094144@outlook.com>
2018-05-11 08:04:35 +0200

    Dark Theme

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
-import 'vue-material/dist/theme/default.css'
+import 'vue-material/dist/theme/default-dark.css'
 
 Vue.use(VueMaterial);
 
